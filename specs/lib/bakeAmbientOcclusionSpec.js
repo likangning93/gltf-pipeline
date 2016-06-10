6883fe4b4f836141c36f42295f9251348646d4fc
Kangning Li <likangning93@gmail.com>
2016-06-10 14:24:45 -0400

    added a basic spec for what's written so far. TODO: refine

--- specs/lib/bakeAmbientOcclusionSpec.js
+++ specs/lib/bakeAmbientOcclusionSpec.js
@@ -19,6 +19,152 @@ var gltfPath = './specs/data/boxTexturedUnoptimized/CesiumTexturedBoxTest.gltf';
 describe('bakeAmbientOcclusion', function() {
     var boxGltf;
 
+    var indices = [0,1,2,0,2,3];
+    var indicesBuffer = new Buffer(indices.length * 2);
+    for (var i = 0; i < indices.length; i++) {
+        indicesBuffer.writeUInt16LE(indices[i], i * 2);
+    }
+    var positions = [
+        0,0,0,
+        0,1,0,
+        1,1,0,
+        1,0,0
+    ];
+    var normals = [
+        0,0,1,
+        0,0,1,
+        0,0,1,
+        0,0,1
+    ];
+    var uvs = [
+        0.2,0.2,
+        0.8,0.2,
+        0.8,0.8,
+        0.2,0.8
+    ];
+    var positionsBuffer = new Buffer(positions.length * 4);
+    for (var i = 0; i < positions.length; i++) {
+        positionsBuffer.writeFloatLE(positions[i], i * 4);
+    }
+    var normalsBuffer = new Buffer(normals.length * 4);
+    for (var i = 0; i < normals.length; i++) {
+        normalsBuffer.writeFloatLE(normals[i], i * 4);
+    }
+    var uvsBuffer = new Buffer(uvs.length * 4);
+    for (var i = 0; i < uvs.length; i++) {
+        uvsBuffer.writeFloatLE(uvs[i], i * 4);
+    }
+
+    var dataBuffer = Buffer.concat([indicesBuffer, positionsBuffer, normalsBuffer, uvsBuffer]);
+
+    var testGltf = {
+        "accessors": {
+            "accessor_index": {
+                "bufferView": "index_view",
+                "byteOffset": 0,
+                "componentType": 5123,
+                "count": 6,
+                "type": "SCALAR"
+            },
+            "accessor_position": {
+                "bufferView": "position_view",
+                "byteOffset": 0,
+                "componentType": 5126,
+                "count": 4,
+                "type": "VEC3"
+            },
+            "accessor_normal": {
+                "bufferView": "normal_view",
+                "byteOffset": 0,
+                "componentType": 5126,
+                "count": 4,
+                "type": "VEC3"
+            },
+            "accessor_uv": {
+                "bufferView": "uv_view",
+                "byteOffset": 0,
+                "componentType": 5126,
+                "count": 4,
+                "type": "VEC2"
+            }
+        },
+        "bufferViews": {
+            "index_view": {
+                "buffer": "buffer_0",
+                "byteOffset": 0,
+                "byteLength": 6 * 2,
+                "target": 34963
+            },
+            "position_view": {
+                "buffer": "buffer_0",
+                "byteOffset": 6 * 2,
+                "byteLength": 4 * 3 * 4,
+                "target": 34962
+            },
+            "normal_view": {
+                "buffer": "buffer_0",
+                "byteOffset": 2 * 6 * 2,
+                "byteLength": 4 * 3 * 4,
+                "target": 34962
+            },
+            "uv_view": {
+                "buffer": "buffer_0",
+                "byteOffset": 3 * 6 * 2,
+                "byteLength": 4 * 2 * 4,
+                "target": 34962
+            }
+        },
+        "buffers": {
+            "buffer_0": {
+                "uri": "data:",
+                "byteLength": indices.length * 2 + (positions.length + normals.length + uvs.length) * 4,
+                "extras": {
+                    "_pipeline": {
+                        "source": dataBuffer
+                    }
+                }
+            }
+        },
+        "scene": "defaultScene",
+        "scenes": {
+            "defaultScene": {
+                "nodes": [
+                    "squareNode"
+                ]
+            }
+        },
+        "nodes": {
+            "squareNode": {
+                "children": [],
+                "matrix": [
+                    2, 0, 0, 0,
+                    0, 2, 0, 0,
+                    0, 0, 2, 0,
+                    0, 0, 0, 1
+                ],
+                "meshes": [
+                    "mesh_square"
+                ],
+                "name": "square"
+            }
+        },
+        "meshes": {
+            "mesh_square": {
+                "name": "square",
+                "primitives": [
+                    {
+                        "attributes": {
+                            "POSITION": "accessor_position",
+                            "NORMAL": "accessor_normal",
+                            "TEXCOORD_0": "accessor_uv"
+                        },
+                        "indices": "accessor_index"
+                    }
+                ]
+            }
+        }
+    };
+
     beforeAll(function(done) {
         fs.readFile(gltfPath, function(err, data) {
             if (err) {
@@ -35,6 +181,15 @@ describe('bakeAmbientOcclusion', function() {
                 });
             }
         });
+    });
+
+    it('correctly processes a basic 2-triangle square primitive', function() {
+        var scene = testGltf.scenes[testGltf.scene];
+        var options = {
+            "rayDepth" : 0.1,
+            "resolution" : 10
+        }
+        var rayTracerScene = generateRaytracerScene(scene, testGltf, options);
     });
     
 });
